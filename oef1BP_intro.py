217cd293c0a63274b907ec8a9c40775005e847b3
woutdebaere <wout.de.baere@student.howest.Be>
2017-09-26 11:37:42 +0200

    een test voor github

---
+++ oef1BP_intro.py
@@ -0,0 +1,8 @@
+#naam = input("geef mij jouw naam")
+
+
+
+basis = float(input("hoevoeel cm is de basis van een driehoek"))
+hoogte = float(input("wat is de basis van de driehoek"))
+
+print(basis * hoogte /2)
---
+++ oef2BP_intro.py
@@ -0,0 +1,6 @@
+dagen = int(input("geef een aantal dagen op?"))
+uren = int(input("geef een aantal uren op"))
+minuten = int(input("geef een aantal minuten op"))
+seconden = int(input("geef een aantal seconden op"))
+
+print(dagen*24*60*60 + uren*60*60 + minuten*60 + seconden)
